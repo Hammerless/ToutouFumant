f53f1bd918604e689680535613349624d4723590
ByJfMarie <jf.marie@outlook.fr>
2021-08-22 15:51:40 +0200

    Add index menuContreoller

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
 use App\Http\Controllers\API\BookController;
 use App\Http\Controllers\API\UserController;
-use Illuminate\Support\Facades\Route;
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+
+
+Route::apiResource('menus', MenuController::class);
